e803685bdbd3ab474f4781a6fcd58a04ee8a8f04
ANONIMATURE <qui1234@wp.pl>
2024-12-06 02:56:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
-// By VishwaGauravIn (https://itsvg.in)
-
 const GenAI = require("@google/generative-ai");
 const { TwitterApi } = require("twitter-api-v2");
 const SECRETS = require("./SECRETS");
-
 const twitterClient = new TwitterApi({
   appKey: SECRETS.APP_KEY,
   appSecret: SECRETS.APP_SECRET,
   accessToken: SECRETS.ACCESS_TOKEN,
-  accessSecret: SECRETS.ACCESS_SECRET,
-});
-
+  accessSecret: SECRETS.ACCESS_SECRET,});
 const generationConfig = {
-  maxOutputTokens: 400,
-};
+  maxOutputTokens: 400,};
 const genAI = new GenAI.GoogleGenerativeAI(SECRETS.GEMINI_API_KEY);
-
 async function run() {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({
     model: "gemini-pro",
-    generationConfig,
-  });
-
-  // Write your prompt here
-  const prompt = "The tone should be conversational, helpful, and slightly persuasive to encourage clicks or engagement. Content should be concise and attention-grabbing.  say it in polish language
-Use the provided Discord link consistently: https://discord.gg/NKtRwQDp. Keep sentences concise, below 280 characters for Twitter compatibility.Alternate between promotional content, educational advice, and engaging tips.
-  ";
-
+    generationConfig,});
+  const prompt = "Stwórz chwytliwe i krótkie zdanie promujące Discorda dla maturzystów. Skup się na zachęceniu do dołączenia, podkreślając dostęp do materiałów, arkuszy i wsparcia przed maturą. Dodaj link: https://discord.gg/NKtRwQDp.";
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
   console.log(text);
-  sendTweet(text);
-}
-
+  sendTweet(text);}
 run();
-
-async function sendTweet(tweetText) {
-  try {
+async function sendTweet(tweetText) { try {
     await twitterClient.v2.tweet(tweetText);
     console.log("Tweet sent successfully!");
   } catch (error) {
